56010e97001e325ceabf365bc9a729ab1f339d99
Gabriel Redig <glredig@gmail.com>
2016-05-28 21:06:48 -0700

    allow ordering by category; allow org change

--- app/js/controllers/repos_controller.js
+++ app/js/controllers/repos_controller.js
@@ -1,21 +1,30 @@
 app.controller('ReposController', function($scope, $q, repoService) {
 	$scope.repos = [];
 	$scope.commits = [];
-	$scope.org = 'Netflix'
+	$scope.org = 'Netflix';
+	
 
-	repoService.getRepos($scope.org)
-		.then(function(repos) {
-			$scope.repos = repos.data;
+	$scope.init = function() {
+		getRepos($scope.org);
+		$scope.org_edit = false;
+		$scope.repo_filter = '-forks_count';
+	}
 
-			console.log('repos', $scope.repos);
-		});
+	function getRepos(org) {
+		repoService.getRepos(org)
+			.then(function(repos) {
+				$scope.repos = repos.data;
+
+				// console.log('repos', $scope.repos);
+			});	
+	}
 
 	function getCommits(repo) {
 		repoService.getCommits($scope.org, repo) 
 			.then(function(commits) {
 				$scope.commits = commits.data;
 
-				console.log($scope.commits);
+				console.log('commits', $scope.commits);
 			});
 	}
 
@@ -27,5 +36,27 @@ app.controller('ReposController', function($scope, $q, repoService) {
 
 	$scope.deselectRepo = function() {
 		$scope.selected_repo = undefined;
+	}
+
+	$scope.toggleEdit = function() {
+		if ($scope.org_edit) {
+			getRepos($scope.org);
+		}
+
+		$scope.org_edit = !$scope.org_edit;
+	}
+
+	$scope.updateFilter = function(filter) {
+		if ($scope.repo_filter == filter) {
+			if (filter.substring(0, 1) == '-') {
+				$scope.repo_filter = filter.substring(0);
+			}
+			else {
+				$scope.repo_filter = '-' + filter;
+			}
+		}
+		else {
+			$scope.repo_filter = filter;
+		}
 	}
 });
